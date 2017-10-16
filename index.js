74bdda05907a9377cbb381e6300c04fa043d9903
bpaksoy <paksoybanu83@gmail.com>
2017-10-16 11:56:40 -0400

    Added express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const env = "development";
 const config = require("./knexfile.js")[env];
 const knex = require("knex")(config);
+
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, function(){
+  console.log("Listening on 3000...");
+})
